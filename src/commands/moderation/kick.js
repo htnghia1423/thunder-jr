6894df2310f9aae3d19c99ed6297fafd21549d53
Thunder <htn10a2@gmail.com>
2024-12-24 19:28:33 +0700

    add: ban & kick command

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -27,60 +27,60 @@ module.exports = {
     }
 
     if (targetUser.id === interaction.guild.ownerId) {
-      await interaction.editReply("You cannot ban the server owner.");
+      await interaction.editReply("You cannot kick the server owner.");
       return;
     }
 
     const targetUserRolePosition = targetUser.roles.highest.position; //Highest role position of the target user
-    const requestUserRolePosition = interaction.member.roles.highest.position; //Highest role position of the user who requested the ban
+    const requestUserRolePosition = interaction.member.roles.highest.position; //Highest role position of the user who requested the kick
     const botRolePosition = interaction.guild.members.me.roles.highest.position; //Highest role position of the bot
 
     if (targetUserRolePosition >= requestUserRolePosition) {
       await interaction.editReply(
-        "You cannot ban a member who has a higher or equal role position."
+        "You cannot kick a member who has a higher or equal role position."
       );
       return;
     }
 
     if (targetUserRolePosition >= botRolePosition) {
       await interaction.editReply(
-        "I cannot ban a member who has a higher or equal role position than me."
+        "I cannot kick a member who has a higher or equal role position than me."
       );
       return;
     }
 
-    //Ban the user
+    //kick the user
     try {
-      await targetUser.ban({ reason });
+      await targetUser.kick(reason);
       await interaction.editReply(
-        `Successfully banned ${targetUser}!\nReason: ${reason}`
+        `Successfully kicked ${targetUser}!\nReason: ${reason}`
       );
     } catch (error) {
       console.log("🪲 Có biến rồi Đại vương ơi: ", error);
       await interaction.editReply(
-        "There was an error trying to ban that user."
+        "There was an error trying to kick that user."
       );
     }
   },
-  name: "ban",
-  description: "Bans a member from the server!",
+  name: "kick",
+  description: "Kicks a member from the server!",
   //   devOnly: Boolean,
   //   testOnly: Boolean,
   // deleted: Boolean,
   options: [
     {
       name: "target-user",
-      description: "The user you want to ban.",
+      description: "The user you want to kick.",
       required: true,
       type: ApplicationCommandOptionType.Mentionable,
     },
     {
       name: "reason",
-      description: "The reason for banning.",
+      description: "The reason for kicking.",
       type: ApplicationCommandOptionType.String,
     },
   ],
 
-  permissionsRequired: [PermissionFlagsBits.BanMembers],
-  botPermissions: [PermissionFlagsBits.BanMembers],
+  permissionsRequired: [PermissionFlagsBits.KickMembers],
+  botPermissions: [PermissionFlagsBits.KickMembers],
 };
