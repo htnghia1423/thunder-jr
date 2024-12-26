894cbd3e000279e3b976cce5db2085b6fd09f22d
Thunder <htn10a2@gmail.com>
2024-12-26 18:40:25 +0700

    add: connect to database for bot
    Using MongoDB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Client, IntentsBitField } = require("discord.js");
 const eventHandler = require("./handlers/eventHandler");
+const mongoose = require("mongoose");
 
 const client = new Client({
   intents: [
@@ -11,6 +12,16 @@ const client = new Client({
   ],
 });
 
-eventHandler(client);
+(async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
 
-client.login(process.env.DISCORD_TOKEN);
+    eventHandler(client);
+
+    client.login(process.env.DISCORD_TOKEN);
+  } catch (error) {
+    console.log("Error connecting to MongoDB: ", error);
+  }
+})();
