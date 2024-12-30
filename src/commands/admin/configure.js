a986ae1e0b0fa3116f6c4a2ed2956cca8f28600e
Thunder <htn10a2@gmail.com>
2024-12-30 15:31:35 +0700

    add: learn about subcommand and subcommand group

---
+++ src/commands/admin/configure.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  run: ({ interaction }) => {
+    const subcommandGroup = interaction.options.getSubcommandGroup();
+    const subcommand = interaction.options.getSubcommand();
+
+    if (subcommandGroup === "user") {
+      if (subcommand === "role") {
+        const targetUser = interaction.options.getUser("target-user");
+        const role = interaction.options.getRole("role");
+
+        interaction.reply(`Configured ${targetUser} with role ${role}.`);
+      }
+    }
+  },
+
+  data: new SlashCommandBuilder()
+    .setName("configure")
+    .setDescription("Configure some stuff....")
+    .addSubcommandGroup((subcommandGroup) =>
+      subcommandGroup
+        .setName("user")
+        .setDescription("Configure a user.")
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("role")
+            .setDescription("Configure a user's role.")
+            .addUserOption((option) =>
+              option
+                .setName("target-user")
+                .setDescription("The user you want to configure.")
+                .setRequired(true)
+            )
+            .addRoleOption((option) =>
+              option
+                .setName("role")
+                .setDescription("The role you want to give to the user.")
+                .setRequired(true)
+            )
+        )
+    ),
+
+  // deleted: true,
+};
